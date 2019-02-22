0417adef23ddf443f1821fbdf74cd78fa0863244
rfgm6 <ruifgmonteiro@gmail.com>
2019-02-22 01:01:49 +0000

    ping

--- api/scraper.py
+++ api/scraper.py
@@ -1,40 +1,49 @@
 from api.handle_requests import simple_get
 from bs4 import BeautifulSoup
-from lxml import etree, html
-
 
 # Scraping implementation. Should return a list of dictionaries.
 def scrap_page():
+	# Scraping base url.
 	base_url = 'http://francesinhas.com'
 	raw_html = simple_get(base_url)
 	soup = BeautifulSoup(raw_html, 'html.parser')
-	#print(soup)
 
-	# Get all text from div top-places.
+	# Isolate target code.
 	top_places = soup.find(class_='top-places')
 	restaurants = top_places.find_all('a')
+
+	# Initialize dict, list of dicts (will be used as response) and rank.
 	d = {}
 	d_list = []
-	position = 0
+	rank = 0
+
+	# Loop over the detected top restaurants.
 	for restaurant in restaurants:
-		position = position + 1
-		# Fetch do nome e posição do restaurante.
+		rank = rank + 1
+		# Get restaurant name and rank.
 		for restaurant_name in restaurant:
 			d['name'] = restaurant_name
-			d['position'] = position
-		#	print(j)
+			d['rank'] = rank
+
+			# Scraping each of top restaurants specific web page.
 			restaurant_url = base_url + restaurant.get('href')
 			restaurant_html = simple_get(restaurant_url)
+
+			# Generate bs object.
 			restaurant_soup = BeautifulSoup(restaurant_html, 'html.parser')
-			addresses = restaurant_soup.find(class_='address')
-			address = str(addresses.find_all('p'))
-			new_soup = BeautifulSoup(address, 'html.parser')
-			pretty_html = new_soup.prettify()
-			pretty_string = str(pretty_html)
-			print(pretty_string)
-			d['address'] = pretty_string
+
+			# Find all p's.
+			address = restaurant_soup.find('div', class_='address').p
+
+			sep = '<br'
+			rest = str(address).split(sep, 1)[0]
+			new_sep = '<p>'
+			new_rest = rest.split(new_sep, 1)[1]
+			final = new_rest.strip()
+
+			d['address'] = final
+
 			d_list.append(d.copy())
-		#	print(d)
 
 	return d_list
 
